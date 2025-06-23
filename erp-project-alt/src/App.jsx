2ed4b25a42b0665205f3f0303ba5afa9beb1ecaf
Samu <shaibusamuel423@gmail.com>
2025-06-24 00:58:24 +0100

    should work now

--- erp-project-alt/src/App.jsx
+++ erp-project-alt/src/App.jsx
@@ -2,9 +2,7 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes'
 
 function App() {
-  return 
-  <h1>Test only</h1>  
-  <RouterProvider router={router} />
+  return <RouterProvider router={router} />
 }
 
 export default App
